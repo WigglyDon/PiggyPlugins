61e153225b4dfe6cf235847bd244821f2f5fee89
wigglyDon <donfather1000@gmail.com>
2024-02-10 18:08:16 -0700

    enforce code style

--- AutoVardorvis/src/main/java/com/wigglydonplugins/AutoVardorvis/AutoVardorvisPlugin.java
+++ AutoVardorvis/src/main/java/com/wigglydonplugins/AutoVardorvis/AutoVardorvisPlugin.java
@@ -1,152 +1,196 @@
 package com.wigglydonplugins.AutoVardorvis;
 
 import com.example.EthanApiPlugin.Collections.Inventory;
+import com.example.EthanApiPlugin.Collections.NPCs;
 import com.example.InteractionApi.InventoryInteraction;
+import com.google.inject.Inject;
 import com.google.inject.Provides;
-import com.wigglydonplugins.AutoVardorvis.state.StateHandler.State;
+import com.piggyplugins.PiggyUtils.API.PrayerUtil;
 import com.wigglydonplugins.AutoVardorvis.state.StateHandler;
+import com.wigglydonplugins.AutoVardorvis.state.StateHandler.State;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.*;
-import net.runelite.api.events.*;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.Prayer;
+import net.runelite.api.Projectile;
+import net.runelite.api.Varbits;
+import net.runelite.api.events.ChatMessage;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.ProjectileMoved;
+import net.runelite.api.events.VarbitChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.SpriteManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
-import com.google.inject.Inject;
 
 @Slf4j
 @PluginDescriptor(
-        name = "<html><font color=\"#FF0000\">[WD]</font>Auto Vardorvis</html>",
-        description = "Automated vardorvis killer"
+    name = "<html><font color=\"#FF0000\">[WD]</font>Auto Vardorvis</html>",
+    description = "Automated vardorvis killer"
 )
 public class AutoVardorvisPlugin extends Plugin {
 
-    private static final int RANGE_PROJECTILE = 2521;
-    private Projectile rangeProjectile;
-    private int rangeTicks = 0;
-    private int rangeCooldown = 0;
+  private static final int RANGE_PROJECTILE = 2521;
+  private Projectile rangeProjectile;
+  private int rangeTicks = 0;
+  private int rangeCooldown = 0;
 
 
-    int totalKills = 0;
-    long startTime = System.currentTimeMillis();
-    long elapsedTime = 0;
-    boolean running = false;
-    int tickDelay = 0;
+  int totalKills = 0;
+  long startTime = System.currentTimeMillis();
+  long elapsedTime = 0;
+  boolean running = false;
+  int tickDelay = 0;
 
-    State botState = null;
-    private boolean drankSuperCombat = false;
+  State botState = null;
+  private boolean drankSuperCombat = false;
 
-    @Inject
+  @Inject
+  private Client client;
+  @Inject
+  private SpriteManager spriteManager;
+  @Inject
+  private OverlayManager overlayManager;
+  @Inject
+  private AutoVardorvisOverlay overlay;
+  @Inject
+  private AutoVardorvisConfig config;
+
+  @Provides
+  private AutoVardorvisConfig getConfig(ConfigManager configManager) {
+    return configManager.getConfig(AutoVardorvisConfig.class);
+  }
+
+  @Override
+  protected void startUp() throws Exception {
+    startTime = System.currentTimeMillis();
+    overlayManager.add(overlay);
+    running = client.getGameState() == GameState.LOGGED_IN;
+    botState = State.FIGHTING;
+  }
+
+  @Override
+  protected void shutDown() throws Exception {
+    totalKills = 0;
+    overlayManager.remove(overlay);
+    drankSuperCombat = false;
+    running = false;
+    botState = null;
+  }
+
+  @Getter
+  @Setter
+  public static class MainClassContext {
+
     private Client client;
-    @Inject
-    private SpriteManager spriteManager;
-    @Inject
-    private OverlayManager overlayManager;
-    @Inject
-    private AutoVardorvisOverlay overlay;
-    @Inject
     private AutoVardorvisConfig config;
+    private boolean drankSuperCombat;
 
-    @Provides
-    private AutoVardorvisConfig getConfig(ConfigManager configManager) {
-        return configManager.getConfig(AutoVardorvisConfig.class);
+    public MainClassContext(Client client, AutoVardorvisConfig config, boolean drankSuperCombat) {
+      this.client = client;
+      this.config = config;
+      this.drankSuperCombat = drankSuperCombat;
+    }
+  }
+
+  private void handleBotState(State botState) {
+    if (botState == null) {
+      System.out.println("Null state...");
+      return;
+    }
+    MainClassContext context = new MainClassContext(client, config, drankSuperCombat);
+    StateHandler stateHandler = new StateHandler();
+    stateHandler.handleState(botState, context);
+  }
+
+  @Subscribe
+  private void onGameTick(GameTick event) {
+
+    long currentTime = System.currentTimeMillis();
+    elapsedTime = currentTime - startTime;
+    overlay.updateKillsPerHour();
+
+    if (running) {
+      if (tickDelay > 0) {
+        tickDelay--;
+        return;
+      }
+      handleBotState(botState);
+      if (isInFight()) {
+        autoPray();
+      }
+    }
+  }
+
+  @Subscribe
+  private void onVarbitChanged(VarbitChanged event) {
+    if (event.getVarbitId() == Varbits.DIVINE_SUPER_COMBAT) {
+      drankSuperCombat = true;
+      if (event.getValue() <= 10) {
+        Inventory.search().nameContains("Divine super combat").first().ifPresent(potion -> {
+          InventoryInteraction.useItem(potion, "Drink");
+        });
+      }
+    }
+  }
+
+  @Subscribe
+  private void onProjectileMoved(ProjectileMoved event) {
+    if (client.getGameState() != GameState.LOGGED_IN) {
+      return;
     }
 
-    @Override
-    protected void startUp() throws Exception {
-        startTime = System.currentTimeMillis();
-        overlayManager.add(overlay);
-        running = client.getGameState() == GameState.LOGGED_IN;
-        botState = State.FIGHTING;
+    Projectile projectile = event.getProjectile();
+
+    if (projectile.getId() == RANGE_PROJECTILE) {
+      if (rangeProjectile == null && rangeCooldown == 0) {
+        rangeTicks = 4;
+        rangeProjectile = projectile;
+      }
+    }
+  }
+
+  private void autoPray() {
+    if (rangeTicks > 0) {
+      rangeTicks--;
+      if (rangeTicks == 0) {
+        rangeCooldown = 3;
+      }
     }
 
-    @Override
-    protected void shutDown() throws Exception {
-        totalKills = 0;
-        overlayManager.remove(overlay);
-        drankSuperCombat = false;
-        running = false;
-        botState = null;
+    if (rangeTicks == 0) {
+      if (rangeCooldown > 0) {
+        rangeCooldown--;
+      }
     }
+    handleRangeFirstGameTick();
+  }
 
-    @Getter
-    @Setter
-    public static class MainClassContext {
-        private Client client;
-        private AutoVardorvisConfig config;
-        private int rangeTicks;
-        private int rangeCooldown;
-        private boolean drankSuperCombat;
-
-        public MainClassContext(Client client, AutoVardorvisConfig config, int rangeTicks, int rangeCooldown, boolean drankSuperCombat) {
-            this.client = client;
-            this.config = config;
-            this.rangeTicks = rangeTicks;
-            this.rangeCooldown = rangeCooldown;
-            this.drankSuperCombat = drankSuperCombat;
-        }
+  private void handleRangeFirstGameTick() {
+    if (rangeTicks > 0) {
+      if (!PrayerUtil.isPrayerActive(Prayer.PROTECT_FROM_MISSILES)) {
+        PrayerUtil.togglePrayer(Prayer.PROTECT_FROM_MISSILES);
+      }
+    } else {
+      if (!PrayerUtil.isPrayerActive(Prayer.PROTECT_FROM_MELEE)) {
+        PrayerUtil.togglePrayer(Prayer.PROTECT_FROM_MELEE);
+      }
     }
+  }
 
-    private void handleBotState(State botState) {
-        if (botState == null) {
-            System.out.println("Null state...");
-            return;
-        }
-        MainClassContext context = new MainClassContext(client, config, rangeTicks, rangeCooldown, drankSuperCombat);
-        StateHandler stateHandler = new StateHandler();
-        stateHandler.handleState(botState, context);
+  @Subscribe
+  private void onChatMessage(ChatMessage e) {
+    if (e.getMessage().contains("Your Vardorvis kill count is:")) {
+      totalKills++;
     }
-    @Subscribe
-    private void onGameTick(GameTick event) {
-        long currentTime = System.currentTimeMillis();
-        elapsedTime = currentTime - startTime;
-        overlay.updateKillsPerHour();
+  }
 
-        if (running) {
-            if (tickDelay > 0) {
-                tickDelay--;
-                return;
-            }
-            handleBotState(botState);
-        }
-    }
-
-    @Subscribe
-    private void onVarbitChanged(VarbitChanged event) {
-        if (event.getVarbitId() == Varbits.DIVINE_SUPER_COMBAT) {
-            drankSuperCombat = true;
-            if (event.getValue() <= 10) {
-                Inventory.search().nameContains("Divine super combat").first().ifPresent(potion -> {
-                    InventoryInteraction.useItem(potion, "Drink");
-                });
-            }
-        }
-    }
-
-    @Subscribe
-    private void onProjectileMoved(ProjectileMoved event) {
-        if (client.getGameState() != GameState.LOGGED_IN) {
-            return;
-        }
-
-        Projectile projectile = event.getProjectile();
-
-        if (projectile.getId() == RANGE_PROJECTILE) {
-            if (rangeProjectile == null && rangeCooldown == 0) {
-                rangeTicks = 4;
-                rangeProjectile = projectile;
-            }
-        }
-    }
-
-    @Subscribe
-    private void onChatMessage(ChatMessage e) {
-        if (e.getMessage().contains("Your Vardorvis kill count is:")) {
-            totalKills ++;
-        }
-    }
+  private boolean isInFight() {
+    return client.isInInstancedRegion() && NPCs.search().nameContains("Vardorvis").nearestToPlayer()
+        .isPresent();
+  }
 }
--- AutoVardorvis/src/main/java/com/wigglydonplugins/AutoVardorvis/state/botStates/FightingState.java
+++ AutoVardorvis/src/main/java/com/wigglydonplugins/AutoVardorvis/state/botStates/FightingState.java
@@ -11,215 +11,195 @@ import com.example.Packets.MovementPackets;
 import com.example.Packets.WidgetPackets;
 import com.piggyplugins.PiggyUtils.API.PrayerUtil;
 import com.wigglydonplugins.AutoVardorvis.AutoVardorvisConfig;
-import net.runelite.api.*;
-import net.runelite.api.coords.WorldPoint;
-
+import com.wigglydonplugins.AutoVardorvis.AutoVardorvisPlugin.MainClassContext;
 import java.util.List;
 import java.util.Optional;
-
-import com.wigglydonplugins.AutoVardorvis.AutoVardorvisPlugin.MainClassContext;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.NPC;
+import net.runelite.api.Prayer;
+import net.runelite.api.Skill;
+import net.runelite.api.TileObject;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.widgets.Widget;
 
 public class FightingState {
 
-    private final String VARDORVIS = "Vardorvis";
-    private Client client;
-    private AutoVardorvisConfig config;
-    private int rangeTicks;
-    private int rangeCooldown;
-    WorldPoint safeTile = null;
-    private WorldPoint lastKnownSafeTile;
-    private boolean drankSuperCombat;
-    WorldPoint axeMoveTile = null;
-    private int axeTicks = 0;
+  private final String VARDORVIS = "Vardorvis";
+  private Client client;
+  private static WorldPoint safeTile = null;
+  private static WorldPoint axeMoveTile = null;
+  private boolean drankSuperCombat;
+  private static int axeTicks = 0;
 
-    public void execute(MainClassContext context) {
-        client = context.getClient();
-        config = context.getConfig();
+  public void execute(MainClassContext context) {
+    client = context.getClient();
+    AutoVardorvisConfig config = context.getConfig();
 
-        rangeTicks = context.getRangeTicks();
-        context.setRangeTicks(rangeTicks);
+    drankSuperCombat = context.isDrankSuperCombat();
 
-        rangeCooldown = context.getRangeCooldown();
-        context.setRangeCooldown(rangeCooldown);
+    List<NPC> newAxes = NPCs.search().withId(12225).result();
+    List<NPC> activeAxes = NPCs.search().withId(12227).result();
+    Optional<NPC> vardorvis = NPCs.search().nameContains(VARDORVIS).first();
 
-        drankSuperCombat = context.isDrankSuperCombat();
+    WorldPoint playerTile = client.getLocalPlayer().getWorldLocation();
+    Optional<TileObject> safeRock = TileObjects.search().withAction("Leave").first();
+
+    if (client.getGameState() != GameState.LOGGED_IN || !isInFight(client)) {
+      turnOffPrayers();
+      return;
+    }
+
+    if (!PrayerUtil.isPrayerActive(Prayer.PIETY)) {
+      PrayerUtil.togglePrayer(Prayer.PIETY);
+    }
+    doBloodCaptcha();
+
+    //initial attack
+    if (vardorvis.isPresent() && safeTile != null) {
+      if (vardorvis.get().getWorldLocation().getX() == safeTile.getX() + 4
+          && vardorvis.get().getWorldLocation().getY() == safeTile.getY() - 1
+          && vardorvis.get().getAnimation() == -1
+      ) {
+        vardorvis.ifPresent(npc -> {
+          NPCInteraction.interact(npc, "Attack");
+          if (!drankSuperCombat) {
+            Inventory.search().nameContains("Divine super combat").first().ifPresent(potion -> {
+              InventoryInteraction.useItem(potion, "Drink");
+              drankSuperCombat = true;
+              context.setDrankSuperCombat(true);
+            });
+          }
+        });
+        return;
+      } else if (vardorvis.get().getWorldLocation().getX() != safeTile.getX() + 1) {
+        movePlayerToTile(safeTile);
+      }
+    }
+
+    if (!newAxes.isEmpty()) {
+      newAxes.forEach((axe) -> {
+        if (axe.getWorldLocation().getX() == safeTile.getX() - 1
+            && axe.getWorldLocation().getY() == safeTile.getY() - 1) {
+          handleAxeMove();
+        }
+      });
+    }
+
+    if (!activeAxes.isEmpty()) {
+      activeAxes.forEach((axe) -> {
+        if (axe.getWorldLocation().getX() == safeTile.getX() + 1
+            && axe.getWorldLocation().getY() == safeTile.getY() - 1) {
+          axeTicks = 1;
+          handleAxeMove();
+        }
+      });
+    }
+
+    if (safeRock.isPresent() && safeTile == null) {
+      WorldPoint safeRockLocation = safeRock.get().getWorldLocation();
+      safeTile = new WorldPoint(safeRockLocation.getX() + 6, safeRockLocation.getY() - 10, 0);
+      axeMoveTile = new WorldPoint(safeTile.getX() + 2, safeTile.getY() - 2, 0);
+    }
+
+    if (safeTile != null) {
+      if (playerTile.getX() != safeTile.getX() || playerTile.getY() != safeTile.getY()) {
+        movePlayerToTile(safeTile);
+        return;
+      } else {
+        eat(config.EATAT());
+        drinkPrayer(config.DRINKPRAYERAT());
         context.setDrankSuperCombat(drankSuperCombat);
-
-        List<NPC> newAxes = NPCs.search().withId(12225).result();
-        List<NPC> activeAxes = NPCs.search().withId(12227).result();
-        Optional<NPC> vardorvis = NPCs.search().nameContains(VARDORVIS).first();
-
-        WorldPoint playerTile = client.getLocalPlayer().getWorldLocation();
-        Optional<TileObject> safeRock = TileObjects.search().withAction("Leave").first();
-
-        if (client.getGameState() != GameState.LOGGED_IN || !isInFight(client)) {
-            turnOffPrayers();
-            return;
-        }
-
-        autoPray();
-        if (!PrayerUtil.isPrayerActive(Prayer.PIETY)) {
-            PrayerUtil.togglePrayer(Prayer.PIETY);
-        }
-        doBloodCaptcha();
-
-
-        //initial attack
-        if (vardorvis.isPresent() && safeTile != null) {
-            if (vardorvis.get().getWorldLocation().getX() == safeTile.getX() + 4
-                    && vardorvis.get().getWorldLocation().getY() == safeTile.getY() - 1
-                    && vardorvis.get().getAnimation() == -1
-            ) {
-                vardorvis.ifPresent(npc -> {
-                    NPCInteraction.interact(npc, "Attack");
-                    if (!drankSuperCombat) {
-                        Inventory.search().nameContains("Divine super combat").first().ifPresent(potion -> {
-                            InventoryInteraction.useItem(potion, "Drink");
-                            drankSuperCombat = true;
-                        });
-                    }
-                });
-                return;
-            } else if (vardorvis.get().getWorldLocation().getX() != safeTile.getX() + 1) {
-                movePlayerToTile(safeTile);
-            }
-        }
-
-        if (!newAxes.isEmpty()) {
-            newAxes.forEach((axe) -> {
-                if (axe.getWorldLocation().getX() == safeTile.getX() - 1 && axe.getWorldLocation().getY() == safeTile.getY() - 1) {
-                    handleAxeMove();
-                }
-            });
-        }
-
-        if (!activeAxes.isEmpty()) {
-            activeAxes.forEach((axe) -> {
-                if (axe.getWorldLocation().getX() == safeTile.getX() + 1 && axe.getWorldLocation().getY() == safeTile.getY() - 1) {
-                    axeTicks = 1;
-                    handleAxeMove();
-                }
-            });
-        }
-
-        if (safeRock.isPresent() && safeTile == null) {
-            WorldPoint safeRockLocation = safeRock.get().getWorldLocation();
-            safeTile = new WorldPoint(safeRockLocation.getX() + 6, safeRockLocation.getY() - 10, 0);
-            axeMoveTile = new WorldPoint(safeTile.getX() + 2, safeTile.getY() - 2, 0);
-        }
-
-        if (safeTile != null) {
-            if (playerTile.getX() != safeTile.getX() || playerTile.getY() != safeTile.getY()) {
-                movePlayerToTile(safeTile);
-                return;
-            } else {
-                eat(config.EATAT());
-                drinkPrayer(config.DRINKPRAYERAT());
-            }
-        }
-
-        if (!client.getLocalPlayer().isInteracting()) {
-            NPCs.search().nameContains(VARDORVIS).first().ifPresent(npc -> {
-                NPCInteraction.interact(npc, "Attack");
-            });
-        }
+      }
     }
-    private void handleAxeMove() {
-        switch (axeTicks) {
-            case 0:
-                break;
-            case 1:
-                movePlayerToTile(axeMoveTile);
-                break;
-        }
-        if (axeTicks == 1) {
-            axeTicks = 0;
-        } else {
-            axeTicks++;
-        }
+
+    if (!client.getLocalPlayer().isInteracting()) {
+      NPCs.search().nameContains(VARDORVIS).first().ifPresent(npc -> {
+        NPCInteraction.interact(npc, "Attack");
+      });
     }
-    private void movePlayerToTile(WorldPoint tile) {
+  }
+
+  private void handleAxeMove() {
+
+    switch (axeTicks) {
+
+      case 0:
+        break;
+      case 1:
+
+        movePlayerToTile(axeMoveTile);
+        break;
+    }
+    if (axeTicks == 1) {
+      axeTicks = 0;
+    } else {
+      axeTicks++;
+    }
+  }
+
+  private void movePlayerToTile(WorldPoint tile) {
+    MousePackets.queueClickPacket();
+    MovementPackets.queueMovement(tile);
+  }
+
+  private void doBloodCaptcha() {
+    List<Widget> captchaBlood = Widgets.search().filter(widget -> widget.getParentId() != 9764864)
+        .hiddenState(false).withAction("Destroy").result();
+    if (!captchaBlood.isEmpty()) {
+      captchaBlood.forEach(x -> {
         MousePackets.queueClickPacket();
-        MovementPackets.queueMovement(tile);
+        WidgetPackets.queueWidgetAction(x, "Destroy");
+      });
     }
-    private void doBloodCaptcha() {
-        List<Widget> captchaBlood = Widgets.search().filter(widget -> widget.getParentId() != 9764864).hiddenState(false).withAction("Destroy").result();
-        if (!captchaBlood.isEmpty()) {
-            captchaBlood.forEach(x -> {
-                MousePackets.queueClickPacket();
-                WidgetPackets.queueWidgetAction(x, "Destroy");
-            });
-        }
-    }
-    private boolean isInFight(Client client) {
-        return client.isInInstancedRegion() && NPCs.search().nameContains(VARDORVIS).nearestToPlayer().isPresent();
-    }
-    private void turnOffPrayers() {
-        if (PrayerUtil.isPrayerActive(Prayer.PIETY)) {
-            PrayerUtil.togglePrayer(Prayer.PIETY);
-        }
-        if (PrayerUtil.isPrayerActive(Prayer.PROTECT_FROM_MELEE)) {
-            PrayerUtil.togglePrayer(Prayer.PROTECT_FROM_MELEE);
-        }
-    }
-    private void autoPray() {
-        if (rangeTicks > 0) {
-            rangeTicks--;
-            if (rangeTicks == 0) {
-                rangeCooldown = 3;
-            }
-        }
+  }
 
-        if (rangeTicks == 0) {
-            if (rangeCooldown > 0) {
-                rangeCooldown--;
-            }
-        }
-        handleRangeFirstGameTick();
-    }
-    private void handleRangeFirstGameTick() {
-        if (rangeTicks > 0) {
-            if (!PrayerUtil.isPrayerActive(Prayer.PROTECT_FROM_MISSILES)) {
-                PrayerUtil.togglePrayer(Prayer.PROTECT_FROM_MISSILES);
-            }
-        } else {
-            if (!PrayerUtil.isPrayerActive(Prayer.PROTECT_FROM_MELEE)) {
-                PrayerUtil.togglePrayer(Prayer.PROTECT_FROM_MELEE);
-            }
-        }
-    }
-    private void eat(int at) {
-        if (needsToEat(at)) {
-            Inventory.search().withAction("Eat").result().stream()
-                    .findFirst()
-                    .ifPresentOrElse(food -> InventoryInteraction.useItem(food, "Eat"),
-                            () -> teleToHouse()
-                    );
-        }
-    }
+  private boolean isInFight(Client client) {
+    return client.isInInstancedRegion() && NPCs.search().nameContains(VARDORVIS).nearestToPlayer()
+        .isPresent();
+  }
 
-    private void drinkPrayer(int at) {
-        if (needsToDrinkPrayer(at)) {
-            Inventory.search().nameContains("Prayer potion").result().stream()
-                    .findFirst()
-                    .ifPresentOrElse(prayerPotion -> InventoryInteraction.useItem(prayerPotion, "Drink"),
-                            () -> teleToHouse()
-                    );
-        }
+  private void turnOffPrayers() {
+    if (PrayerUtil.isPrayerActive(Prayer.PIETY)) {
+      PrayerUtil.togglePrayer(Prayer.PIETY);
     }
-    private boolean needsToEat(int at) {
-        return client.getBoostedSkillLevel(Skill.HITPOINTS) <= at;
+    if (PrayerUtil.isPrayerActive(Prayer.PROTECT_FROM_MELEE)) {
+      PrayerUtil.togglePrayer(Prayer.PROTECT_FROM_MELEE);
     }
+  }
+
+  private void eat(int at) {
+    if (needsToEat(at)) {
+      Inventory.search().withAction("Eat").result().stream()
+          .findFirst()
+          .ifPresentOrElse(food -> InventoryInteraction.useItem(food, "Eat"),
+              this::teleToHouse
+          );
+    }
+  }
+
+  private void drinkPrayer(int at) {
+    if (needsToDrinkPrayer(at)) {
+      Inventory.search().nameContains("Prayer potion").result().stream()
+          .findFirst()
+          .ifPresentOrElse(prayerPotion -> InventoryInteraction.useItem(prayerPotion, "Drink"),
+              this::teleToHouse
+          );
+    }
+  }
+
+  private boolean needsToEat(int at) {
+    return client.getBoostedSkillLevel(Skill.HITPOINTS) <= at;
+  }
 
 
-    private void teleToHouse() {
-        InventoryInteraction.useItem("Teleport to house", "Break");
-        drankSuperCombat = false;
-    }
+  private void teleToHouse() {
+    InventoryInteraction.useItem("Teleport to house", "Break");
+    drankSuperCombat = false;
+  }
 
-    private boolean needsToDrinkPrayer(int at) {
-        return client.getBoostedSkillLevel(Skill.PRAYER) <= at;
-    }
+  private boolean needsToDrinkPrayer(int at) {
+    return client.getBoostedSkillLevel(Skill.PRAYER) <= at;
+  }
 }
 
