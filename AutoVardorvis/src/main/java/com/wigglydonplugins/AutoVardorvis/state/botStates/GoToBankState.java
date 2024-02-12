5a9cc99975b5cf175e3f39de0e4bf4bf9f9e301a
wigglyDon <donfather1000@gmail.com>
2024-02-11 22:04:04 -0700

    gotobankstate

--- AutoVardorvis/src/main/java/com/wigglydonplugins/AutoVardorvis/state/botStates/GoToBankState.java
+++ AutoVardorvis/src/main/java/com/wigglydonplugins/AutoVardorvis/state/botStates/GoToBankState.java
@@ -1,26 +1,50 @@
 package com.wigglydonplugins.AutoVardorvis.state.botStates;
 
+import com.example.EthanApiPlugin.Collections.TileObjects;
+import com.example.InteractionApi.InventoryInteraction;
+import com.example.InteractionApi.TileObjectInteraction;
 import com.wigglydonplugins.AutoVardorvis.AutoVardorvisPlugin.MainClassContext;
+import java.util.Optional;
+import net.runelite.api.Client;
+import net.runelite.api.Skill;
+import net.runelite.api.TileObject;
 
 public class GoToBankState {
 
+  public MainClassContext context;
+  public Client client;
+
   public void execute(MainClassContext context) {
-    System.out.println("bank state");
+    this.context = context;
+    this.client = context.getClient();
+    if (!inHouse()) {
+      teleToHouse();
+      return;
+    }
+    if (client.getBoostedSkillLevel(Skill.HITPOINTS) < 99
+        || client.getBoostedSkillLevel(Skill.PRAYER) < 99) {
+      Optional<TileObject> pool = TileObjects.search().nameContains("pool of").withAction("Drink")
+          .first();
+      pool.ifPresent(poolObject -> {
+        TileObjectInteraction.interact(poolObject, "Drink");
+      });
+      return;
+    }
+    if (inHouse()) {
+      Optional<TileObject> portal = TileObjects.search().nameContains("Portal Nexus")
+          .first();
+      portal.ifPresent(portalObject -> {
+        TileObjectInteraction.interact(portalObject, "Lunar Isle");
+      });
+    }
   }
-//              if (client.getBoostedSkillLevel(Skill.HITPOINTS) < config.POOLDRINK().width || client.getBoostedSkillLevel(
-//  Skill.PRAYER
-//                ) < config.POOLDRINK().height
-//            ) {
-//    TileObjects.search().nameContains("pool of").withAction("Drink").first().ifPresent { pool ->
-//        TileObjectInteraction.interact(pool, "Drink")
-//    }
-//    return
-//  }
-//            if (inHouse()) {
-//    TileObjects.search().nameContains(config.PORTAL().toString()).first().ifPresent { portal ->
-//        TileObjectInteraction.interact(portal, config.PORTAL().action())
-//    }
-//    return
-//  }
+
+  private void teleToHouse() {
+    InventoryInteraction.useItem("Teleport to house", "Break");
+  }
+
+  private boolean inHouse() {
+    return !TileObjects.search().nameContains("pool of").result().isEmpty();
+  }
 
 }
