b3602e6faf744dddd34f3d94deb71a4f534537b3
wigglyDon <donfather1000@gmail.com>
2024-02-16 12:36:52 -0700

    update stuff

--- AutoCrafting/src/main/java/com/wigglydonplugins/AutoCrafting/AutoCraftingOverlay.java
+++ AutoCrafting/src/main/java/com/wigglydonplugins/AutoCrafting/AutoCraftingOverlay.java
@@ -15,50 +15,47 @@ import net.runelite.client.ui.overlay.components.LineComponent;
 
 public class AutoCraftingOverlay extends OverlayPanel {
 
-    private final Client client;
-    private final AutoCraftingPlugin plugin;
+  private final Client client;
+  private final AutoCraftingPlugin plugin;
 
 
+  @Inject
+  private AutoCraftingOverlay(Client client, SpriteManager spriteManager,
+      AutoCraftingPlugin plugin) {
+    super(plugin);
+    this.client = client;
+    this.plugin = plugin;
+    setPosition(OverlayPosition.BOTTOM_RIGHT);
+    setLayer(OverlayLayer.ABOVE_WIDGETS);
+  }
 
+  @Override
+  public Dimension render(Graphics2D graphics2D) {
+    panelComponent.getChildren().clear();
 
+    LineComponent tickDelay = buildLine("Tick Delay: ", Integer.toString(plugin.tickDelay));
+    panelComponent.getChildren().add(tickDelay);
 
-    @Inject
-    private AutoCraftingOverlay(Client client, SpriteManager spriteManager, AutoCraftingPlugin plugin) {
-        super(plugin);
-        this.client = client;
-        this.plugin = plugin;
-        setPosition(OverlayPosition.BOTTOM_RIGHT);
-        setLayer(OverlayLayer.ABOVE_WIDGETS);
-    }
+    LineComponent lastCrafted = buildLine("Last Crafted: ", Integer.toString(plugin.lastCrafted));
+    panelComponent.getChildren().add(lastCrafted);
 
-    @Override
-    public Dimension render(Graphics2D graphics2D) {
-        panelComponent.getChildren().clear();
+    return super.render(graphics2D);
+  }
 
-      LineComponent tickDelay = buildLine("Tick Delay: ", Integer.toString(plugin.tickDelay));
-      panelComponent.getChildren().add(tickDelay);
+  private LineComponent buildLine(String left, String right) {
+    return LineComponent.builder()
+        .left(left)
+        .right(right)
+        .leftColor(Color.WHITE)
+        .rightColor(Color.YELLOW)
+        .build();
+  }
 
-      LineComponent playerIdleCounter = buildLine("playerIdleCounter: ", Integer.toString(
-          plugin.playerIdleCounter));
-      panelComponent.getChildren().add(playerIdleCounter);
+  private String formatTime(Long timeInMillis) {
+    long hours = TimeUnit.MILLISECONDS.toHours(timeInMillis);
+    long minutes = TimeUnit.MILLISECONDS.toMinutes(timeInMillis) % 60;
+    long seconds = TimeUnit.MILLISECONDS.toSeconds(timeInMillis) % 60;
 
-        return super.render(graphics2D);
-    }
-
-    private LineComponent buildLine(String left, String right) {
-        return LineComponent.builder()
-                .left(left)
-                .right(right)
-                .leftColor(Color.WHITE)
-                .rightColor(Color.YELLOW)
-                .build();
-    }
-
-    private String formatTime(Long timeInMillis) {
-        long hours = TimeUnit.MILLISECONDS.toHours(timeInMillis);
-        long minutes = TimeUnit.MILLISECONDS.toMinutes(timeInMillis) % 60;
-        long seconds = TimeUnit.MILLISECONDS.toSeconds(timeInMillis) % 60;
-
-        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
-    }
+    return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+  }
 }
--- AutoCrafting/src/main/java/com/wigglydonplugins/AutoCrafting/AutoCraftingPlugin.java
+++ AutoCrafting/src/main/java/com/wigglydonplugins/AutoCrafting/AutoCraftingPlugin.java
@@ -8,14 +8,21 @@ import com.example.Packets.MousePackets;
 import com.example.Packets.WidgetPackets;
 import com.google.inject.Inject;
 import com.google.inject.Provides;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Locale;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.MenuAction;
+import net.runelite.api.MenuEntry;
 import net.runelite.api.Player;
 import net.runelite.api.events.GameTick;
+import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.menus.WidgetMenuOption;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
@@ -37,11 +44,14 @@ public class AutoCraftingPlugin extends Plugin {
   private AutoCraftingConfig config;
   boolean running = false;
   int tickDelay = 0;
+  int lastCrafted = 0;
   private Widget leatherItem = null;
+
   @Provides
   private AutoCraftingConfig getConfig(ConfigManager configManager) {
     return configManager.getConfig(AutoCraftingConfig.class);
   }
+
   @Override
   protected void startUp() throws Exception {
     overlayManager.add(overlay);
@@ -53,50 +63,63 @@ public class AutoCraftingPlugin extends Plugin {
     overlayManager.remove(overlay);
     running = false;
   }
-  int playerIdleCounter = 0;
 
   @Subscribe
   private void onGameTick(GameTick event) {
-    if (running) {
-      if (tickDelay > 0) {
-        tickDelay--;
-        return;
+    if (!running || tickDelay > 0) {
+      // Plugin not running or there's a tick delay
+      tickDelay--;
+      return;
+    }
+    if (lastCrafted > 0) {
+      lastCrafted--;
+    }
+
+    //leather crafting animation
+    if (client.getLocalPlayer().getAnimation() == 1249) {
+      lastCrafted = 7;
+    }
+
+    Inventory.search().withId(config.LEATHER_TYPE().getLeatherType()).first()
+        .ifPresent((leather) -> {
+          leatherItem = leather;
+        });
+
+    if (config.ARMOR_TYPE().getLeatherNeeded() <= Inventory.getItemAmount(
+        config.LEATHER_TYPE().getLeatherType())) {
+
+      if (Inventory.search().withId(config.LEATHER_TYPE().getLeatherType()).result().size()
+          >= config.ARMOR_TYPE().getLeatherNeeded()) {
+
+        if (lastCrafted == 0) {
+
+          Widgets.search().withAction("Make").nameContains(config.ARMOR_TYPE().getArmorType())
+              .first()
+              .ifPresentOrElse((w) -> {
+                System.out.println("try to click: ");
+                System.out.println(w.getName());
+
+                //CHILD ID IS WHERE QUANTITY HAPPENS?
+                WidgetPackets.queueResumePause(w.getId(), 28);
+                lastCrafted = 7;
+              }, () -> {
+                Inventory.search().nameContains("Needle").first().ifPresent(needle -> {
+                  MousePackets.queueClickPacket();
+                  MousePackets.queueClickPacket();
+                  WidgetPackets.queueWidgetOnWidget(needle, leatherItem);
+                });
+              });
+
+        }
+        // else make
+
       }
 
-      Inventory.search().withId(config.LEATHER_TYPE().getLeatherType()).first().ifPresent((leather) -> {
-        leatherItem = leather;
-      });
 
-      if (config.ARMOR_TYPE().getLeatherNeeded() <= Inventory.getItemAmount(config.LEATHER_TYPE().getLeatherType())) {
-
-        // if idle
-        if (playerIdleCounter == 0) {
-          //if on craft menu
-          if (Widgets.search().withTextContains("How many do you wish to make?").first().isPresent()) {
-//            System.out.println("craft x screen");
-            //else craft
-          } else {
-          InventoryInteraction.useItem("Needle", "Use");
-          Inventory.search().nameContains("Needle").first().ifPresent(needle -> {
-            MousePackets.queueClickPacket();
-            MousePackets.queueClickPacket();
-            WidgetPackets.queueWidgetOnWidget(needle, leatherItem);
-            System.out.println("click needle on leather");
-          });
-          }
-
-          // not idle
-        } else if (client.getLocalPlayer().getAnimation() == -1) {
-          //wait for idle
-            playerIdleCounter --;
-        } else playerIdleCounter = 5;
-      } else {
-        //bank here
-        System.out.println("not enough materials left");
+    } else {
+      //bank here
+      System.out.println("not enough materials left");
 //        bank();
-      }
-
     }
   }
-
 }
